72c9a085062bc3dde96d82149f6237a52cc500ac
NikkoES <nikkoeka04@gmail.com>
2018-10-16 07:56:22 +0700

    initial commit

---
+++ app/src/main/java/com/dicoding/nikkoes/recyclerviewapps/MainActivity.java
@@ -0,0 +1,82 @@
+package com.dicoding.nikkoes.recyclerviewapps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvCategory;
+    private ArrayList<President> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rvCategory = (RecyclerView)findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+        list = new ArrayList<>();
+        list.addAll(PresidentData.getListData());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    private void showRecyclerGrid(){
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cardViewPresidentAdapter = new CardViewPresidentAdapter(this);
+        cardViewPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(cardViewPresidentAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String title = null;
+        switch (item.getItemId()){
+            case R.id.action_list:
+                title = "Mode List View";
+                showRecyclerList();
+                break;
+
+            case R.id.action_grid:
+                title = "Mode Grid View";
+                showRecyclerGrid();
+                break;
+
+            case R.id.action_cardview:
+                title = "Mode Card View";
+                showRecyclerCardView();
+                break;
+        }
+        setActionBarTitle(title);
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setActionBarTitle(String title){
+        getSupportActionBar().setTitle(title);
+    }
+}
